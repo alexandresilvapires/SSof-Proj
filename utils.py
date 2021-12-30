a3f9f50726c55f0dd9bac4d9fe4864c684339ad9
alexpires7 <alexandresilvapires16@tecnico.ulisboa.pt>
2021-12-30 00:30:13 +0000

    Added some more utils and ideas

--- utils.py
+++ utils.py
@@ -55,8 +55,9 @@ def getNodesOfType(tree, type):
     elif(tree["ast_type"] == "Call"):
         if(type == "Call"):
             nodes += [tree]
-        for arg in tree["args"]:
-            nodes += getNodesOfType(arg, type)
+        if("args" in tree.keys()):
+            for arg in tree["args"]:
+                nodes += getNodesOfType(arg, type)
         return nodes + getNodesOfType(tree["func"], type)
 
     elif(tree["ast_type"] == "Attribute"):
@@ -111,3 +112,91 @@ def getCallsWithID(tree, id):
             if(f["func"]['attr'] == id):
                 calls = calls + [f]
     return calls
+
+def getAssignmentsFromVarWithID(tree, id):
+    """Given a tree and an ID of a variable, returns the list of assignments where
+        the given variable was used as the value of the assignment"""
+    assignments = []
+    assigns = getNodesOfType(tree, "Assign")
+    
+    for a in assigns:
+        if(a["value"]["ast_type"] == "Name" and a["value"]["id"] == id):
+                assignments = assignments + [a]
+    return assignments
+
+def getVarsAssinedAsTargetWithID(tree, id):
+    """Given a tree and an ID of a variable, returns the list of IDs of vars 
+        that where assigned a value of the given variable ID"""
+    assignments = []
+    assigns = getAssignmentsFromVarWithID(tree, id)
+    
+    for a in assigns:
+        for t in a["targets"]:
+            if(t["value"]["ast_type"] == "Name"):
+                    assignments = assignments + [t["value"]["id"] ]
+    return assignments
+
+def getArgsIDFromCall(tree):
+    """Given a tree that is a call, returns a list of IDs of each arg"""
+
+    vars = []
+    if("args" in tree.keys()):
+        names = getNodesOfType(tree["args"], "Name")
+        for n in names:
+            vars.append(n["id"])
+    return vars
+
+def getTargetIDInAssignment(tree):
+    """Given a tree that is an assignment, returns a list of IDs of the vars that were assigned"""
+
+    vars = []
+    targets = getNodesOfType(tree["targets"], "Name")
+    for t in targets:
+        vars.append(t["id"])
+    return vars
+
+def removeDupesFromList(l):
+    return list(set(l))
+
+def trackTaint(tree, entry_points, sanitization, sinks):
+    """ Given a list of entry points, sanitization functions and sinks,
+        returns true if any reached the sinks.
+        Only checks explicit flows (?)"""
+    tainted_vars = []
+    
+    assigns = getNodesOfType(tree, "Assign")
+    
+    #TODO: CHECK FOR SANITIZATION!!!
+    
+    #Check if assigned value was entry point or tainted vars and add to list
+    for a in assigns:
+        #Check if any entry points where called
+        for e in entry_points:
+            # If any was, add each target to the list of tainted vars
+            if(getCallsWithID(a, e)):
+                tainted_vars = tainted_vars + getTargetIDInAssignment(a)
+                tainted_vars = removeDupesFromList(tainted_vars)
+
+        for t in tainted_vars:
+            # Check for any vars that were attributed the value of a tainted var
+            tainted_vars = tainted_vars + getVarsAssinedAsTargetWithID(a, t)
+            
+            # Check for any vars that were attributed the value of a function whose args were tainted
+            calls = getNodesOfType(a, "Call")
+            for c in calls:
+                args = getArgsIDFromCall(c)
+                if(t in args):
+                    tainted_vars = tainted_vars + getTargetIDInAssignment(a)
+                    tainted_vars = removeDupesFromList(tainted_vars)
+            
+            
+    
+    #Check if any of the tainted values ever reached a sink
+    #TODO: THIS DOES NOT TAKE IN CONSIDERATION ORDER; SHOULD BE INSIDE FORS OF ASSIGNS WITH LINE CONSIDERATION
+    calls = getNodesOfType(tree, "Call")
+    for c in calls:
+        args = getArgsIDFromCall(c)
+        for t in tainted_vars:
+            if(t in args):
+                return True
+    return False
